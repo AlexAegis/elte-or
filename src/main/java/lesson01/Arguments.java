4eaf0318f6755047cb0e949a0d5a05447b5f24e6
Győri Sándor <alexaegis@gmail.com>
2019-02-14 14:39:52 +0100

    added gradle

---
+++ src/main/java/lesson01/Arguments.java
@@ -0,0 +1,42 @@
+package lesson01;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Arrays;
+
+public class Arguments {
+    public static void main(String ...args) {
+        System.out.println(new Arguments().task(args).toString());
+    }
+
+    private ResultPair task(String ...args) {
+        return new ResultPair(args.length, Arrays.stream(args).filter(s -> s.chars().allMatch(Character::isDigit))
+                .mapToInt(Integer::parseInt).sum());
+    }
+
+    class ResultPair {
+        int count;
+        int sum;
+
+        public ResultPair(int count, int sum) {
+            this.count = count;
+            this.sum = sum;
+        }
+
+        @Override
+        public String toString() {
+            return "ResultPair{" +
+                    "count=" + count +
+                    ", sum=" + sum +
+                    '}';
+        }
+    }
+
+    @Test
+    public void test() {
+        ResultPair resultPair = task("alma", "3", "körte", "6");
+        Assert.assertEquals(resultPair.count, 4);
+        Assert.assertEquals(resultPair.sum, 9);
+    }
+}
--- src/main/java/lesson01/PolishNotation.java
+++ src/main/java/lesson01/PolishNotation.java
@@ -6,13 +6,13 @@ public class PolishNotation {
     public static void main(String[] args) {
         String in = "1 2 * 3 +";
         RPN asd = new RPN();/*
-        Arrays.stream(in.split(" ")).reduce(asd, (rpn, s) -> {
-            System.out.println(rpn + " " + s);
-            return asd;
-        });*/
+                             * Arrays.stream(in.split(" ")).reduce(asd, (rpn, s) -> { System.out.println(rpn
+                             * + " " + s); return asd; });
+                             */
     }
 }
- class RPN {
+
+class RPN {
 
     RPN() {
 
