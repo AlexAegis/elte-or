ab0d572b9b8cfc81f1284bc88ea334bdd152dc04
alexaegis <alexaegis@gmail.com>
2019-02-21 10:26:00 +0100

    first task of lesson 2

--- src/main/java/lesson02/Read.java
+++ src/main/java/lesson02/Read.java
@@ -13,7 +13,14 @@ public class Read {
 	List<String> read(String filename) {
 		List<String> result = new ArrayList<>();
 		try (Scanner scn = new Scanner(new File(Read.class.getResource(filename).toURI()))) {
-			scn.forEachRemaining(result::add);
+			List<Coord> coords = new ArrayList<>();
+			while (scn.hasNextLine()) {
+				coords.add(new Coord(scn.nextLine()));
+			}
+
+			Coord closest = coords.stream()
+					.reduce((a, b) -> Coord.center.manhattan(a) < Coord.center.manhattan(b) ? a : b).orElse(null);
+			System.out.println(closest != null ? closest.toString() : "not fount, no points given");
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
--- src/main/java/lesson02/readme.md
+++ src/main/java/lesson02/readme.md
@@ -3,3 +3,5 @@
 Java file handling practice using Scanner
 
 ## 1) [Read text file with the scanner](./Read.java) - [Test](../../../test/java/lesson02/ReadTest.java)
+
+Read coordinates from the input file, then print out the one nearest to the center
