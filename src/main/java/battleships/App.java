115ae3802936e02623ae51a76924a2f83be81c56
Győri Sándor <alexaegis@gmail.com>
2019-03-07 17:50:32 +0100

    version

--- src/main/java/battleships/App.java
+++ src/main/java/battleships/App.java
@@ -4,32 +4,11 @@ import picocli.CommandLine;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.IVersionProvider;
 import picocli.CommandLine.Option;
-import picocli.CommandLine.Parameters;
-import java.awt.Toolkit;
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.PrintStream;
-import java.io.UnsupportedEncodingException;
 import java.net.URL;
-import java.security.MessageDigest;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Enumeration;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.concurrent.Callable;
 import java.util.jar.Attributes;
 import java.util.jar.Manifest;
-import com.googlecode.lanterna.TerminalPosition;
-import com.googlecode.lanterna.TerminalSize;
-import com.googlecode.lanterna.graphics.TextGraphics;
-import com.googlecode.lanterna.screen.Screen;
-import com.googlecode.lanterna.screen.TerminalScreen;
-import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
-import com.googlecode.lanterna.terminal.Terminal;
 
 @Command(name = "App", sortOptions = false, header = {"", "@|cyan  _____     _   _   _     _____ _   _        |@",
 		"@|cyan | __  |___| |_| |_| |___|   __| |_|_|___ ___ |@",
@@ -40,15 +19,11 @@ import com.googlecode.lanterna.terminal.Terminal;
 		subcommands = {Server.class, Client.class}, footer = {"", "Author: AlexAegis"})
 public class App implements Runnable {
 
-
-	@Option(names = {"-s", "--server"}, paramLabel = "mode", description = "Starts the application in server mode")
-	private boolean server;
-
-	@Option(names = {"-c", "--client"}, paramLabel = "mode", description = "Starts the application in client mode")
-	private boolean client;
-
 	@Option(names = {"-?", "-h", "--help"}, paramLabel = "<help>", description = "Displays help", help = true)
 	private boolean help;
+
+	@Option(names = {"-v", "--version"}, versionHelp = true, description = "Prints version")
+	boolean versionRequested;
 
 	public static void main(String[] args) {
 		CommandLine.run(new App(), System.err, args);
@@ -56,41 +31,23 @@ public class App implements Runnable {
 
 	@Override
 	public void run() {
-		if (help && !server && !client) {
+		if (help) {
 			CommandLine.usage(this, System.err);
 			return;
-		} else if (server) {
-		} else if (client) {
-			new Client().run();
 		}
 	}
-
 	static class ManifestVersionProvider implements IVersionProvider {
 		public String[] getVersion() throws Exception {
-			Enumeration<URL> resources = CommandLine.class.getClassLoader().getResources("META-INF/MANIFEST.MF");
+			Enumeration<URL> resources = getClass().getClassLoader().getResources("META-INF/MANIFEST.MF");
 			while (resources.hasMoreElements()) {
-				URL url = resources.nextElement();
 				try {
-					Manifest manifest = new Manifest(url.openStream());
-					if (isApplicableManifest(manifest)) {
-						Attributes attributes = manifest.getMainAttributes();
-						return new String[] {attributes.get(key("Implementation-Title")) + " version \""
-								+ attributes.get(key("Implementation-Version")) + "\""};
-					}
-				} catch (IOException ex) {
-					return new String[] {"Unable to read from " + url + ": " + ex};
+					Manifest manifest = new Manifest(resources.nextElement().openStream());
+					return new String[] {manifest.getMainAttributes().getValue("Build-Version")};
+				} catch (IOException e) {
+					e.printStackTrace();
 				}
 			}
 			return new String[0];
-		}
-
-		private boolean isApplicableManifest(Manifest manifest) {
-			Attributes attributes = manifest.getMainAttributes();
-			return "picocli".equals(attributes.get(key("Implementation-Title")));
-		}
-
-		private static Attributes.Name key(String key) {
-			return new Attributes.Name(key);
 		}
 	}
 }
